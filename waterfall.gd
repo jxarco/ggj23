0d6702b71ad35fb57813ad69ae4f5dc9f1c984eb
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2023-02-04 22:30:00 +0100

    Updated waterfall

--- waterfall.gd
+++ waterfall.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+var total_time = 1.0
+var time_counter = 0.0
 var foaming = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,7 +9,12 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func _process(delta):
+	time_counter += delta
+	%waterfall_body.get_active_material(0).set_shader_parameter("u_current_time", time_counter)
+	print(time_counter)
+	if time_counter >= total_time:
+		%particles.emitting = true
+	
 
 	
