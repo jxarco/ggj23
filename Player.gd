f2662c3d44d1570fe17e2052893b729317fb9b58
Alex <alexroco.30@gmail.com>
2023-02-04 04:50:48 +0100

    interact in action areas

--- Player.gd
+++ Player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
 @export var speed := 2.0
-var gravity := 15.0
+@export var gravity := 15.0
+@export var current_area := World.Area.NONE
 
 @onready var sprite := %Sprite3D
+@onready var state = World.state
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +15,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	
+	if Input.get_action_strength("Interact"):
+		interact()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -39,4 +43,48 @@ func _physics_process(delta):
 #		var collision = get_slide_collision(i)
 #		print(collision)
 	
-	pass
+
+func interact():
+	
+	match current_area:
+		World.Area.MOLE:
+			print("i'm in MOLE area")
+			if state.waterwayDone:
+				print("WATERWAY ALREADY DONE")
+				return
+				
+			if state.sunIsUp:
+				print("MOLE GETS HOT AND MAKES WRONG WATERWAY")
+			elif not state.sunIsUp:
+				print("MOLE MAKES CORRECT WATERWAY")
+				state.waterwayDone = true
+		World.Area.WATER:
+			print("i'm in WATER area")
+			if state.wetGround:
+				print("WATER ALREADY TO THE WATERWAY")
+				return
+				
+			if state.waterwayDone and not state.grassEaten and not state.sunIsUp:
+				print("WATER GOES THROUGH WATERWAY AND GRASS GROW UP")
+				state.wetGround = true
+			elif state.waterwayDone and state.sunIsUp:
+				print("WATER EVAPORATES")
+			elif not state.waterwayDone:
+				print("FLOOD THE GROUND")
+		World.Area.COW:
+			print("i'm in COW area")
+			if state.grassEaten:
+				print("GRASS ALREADY EATEN")
+				return
+				
+			if state.waterwayDone and state.wetGround and state.sunIsUp:
+				print("COW EATS GRASS")
+				state.grassEaten = true
+			elif state.waterwayDone and state.wetGround and not state.sunIsUp:
+				print("COW WAKES UP, GETS ANGRY AND GOES AWAY")
+			elif not state.waterwayDone and state.wetGround:
+				print("COW DRINKS WATER")
+			elif state.waterwayDone and not state.wetGround:
+				print("COW IGNORES EVERYTHING AND GOES AWAY (NO GRASS)")
+		_:
+			print("No area!")
