02321f27e658ef08eaed8574ffef39285a450b8e
Alex <alexroco.30@gmail.com>
2023-02-04 04:12:26 +0100

    mole actor

--- Scene.gd
+++ Scene.gd
@@ -2,6 +2,11 @@ extends Node3D
 
 @onready var state = World.state
 
+#	var waterwayDone : bool = false
+#	var wetGround : bool
+#	var sunIsUp : bool
+#	var grassEaten : bool
+
 func _on_cow_player_entered(player):
 	print("cow")
 	if state.waterwayDone and state.wetGround and state.sunIsUp:
@@ -14,7 +19,18 @@ func _on_cow_player_entered(player):
 	elif state.waterwayDone and not state.wetGround:
 		print("COW IGNORES EVERYTHING AND GOES AWAY (NO GRASS)")
 
-
 func _on_water_player_entered(player):
 	print("water")
 	pass # Replace with function body.
+
+func _on_mole_player_entered(player):
+	print("mole")
+	if state.waterwayDone:
+		print("WATERWAY ALREADY DONE")
+		return
+		
+	if state.sunIsUp:
+		print("MOLE GETS HOT AND MAKES WRONG WATERWAY")
+	elif not state.sunIsUp:
+		print("MOLE MAKES CORRECT WATERWAY")
+		state.waterwayDone = true
