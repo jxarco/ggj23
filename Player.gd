ce70856601fe28f3a85e8ba8b7ab3f1953c2687c
PZerua <pachus.wtf@gmail.com>
2023-02-05 01:27:42 +0100

    waterway implemented

--- Player.gd
+++ Player.gd
@@ -95,27 +95,29 @@ func interact_mole():
 		return
 
 	state.moleOnSurface = true
-
-	if state.sunIsUp:
-		print("MOLE GETS HOT AND MAKES WRONG WATERWAY")
-		$"../GroundParticles".emitting = true
-	elif state.groundFlooded:
-		print("MOLE GETS AWAY AND DOES NOT MAKE THE WATERWAY")
-	elif not state.sunIsUp:
-		print("MOLE MAKES CORRECT WATERWAY")
-		$"../GroundParticles".emitting = true
-		state.actionSequence.push_back("MOLE")
-		state.waterwayDone = true
 		
 	# Mole returns home
 	var timer = Timer.new()
 	add_child(timer)
 	timer.connect("timeout", func ():
+		
+		if state.sunIsUp:
+			print("MOLE GETS HOT AND MAKES WRONG WATERWAY")
+			$"../GroundParticles".emitting = true
+		elif state.groundFlooded:
+			print("MOLE GETS AWAY AND DOES NOT MAKE THE WATERWAY")
+		elif not state.sunIsUp:
+			print("MOLE MAKES CORRECT WATERWAY")
+			$"../waterway".start_waterway()
+			$"../GroundParticles".emitting = true
+			state.actionSequence.push_back("MOLE")
+			state.waterwayDone = true
+		
 		state.moleOnSurface = false
 		state.moleReturns = true
 		remove_child(timer)
 	)
-	timer.start(5.0)
+	timer.start(2.0)
 
 func interact_water():
 	if state.wetGround or state.groundFlooded:
--- level_scenes/waterway/waterway.gd
+++ level_scenes/waterway/waterway.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-var waterway_steps : int = 2
+var waterway_steps : int = 3
 var current_step : int = waterway_steps
 
 # Called when the node enters the scene tree for the first time.
@@ -13,19 +13,24 @@ func _process(delta):
 	pass
 
 func start_waterway():
-	%Timer.start()
+	%Timer.start(3)
 
 func _on_timer_timeout():
 	
 	current_step = current_step - 1
 	
-	if current_step == 1:
+	if current_step == 2:
 		%waterway_0.hide()
 		%waterway_1.show()
-		
-	if current_step == 0:
+		%Timer.start(1)
+	
+	if current_step == 1:
 		%waterway_1.hide()
 		%waterway_2.show()
-		%Timer.stop()
+		%Timer.start(1)
+		
+	if current_step == 0:
+		%waterway_2.hide()
+		%waterway_3.show()
 	
 	pass # Replace with function body.
